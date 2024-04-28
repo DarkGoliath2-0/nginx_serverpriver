40918dd441690e0055ab8cafcfa9121686393911
DarkGoliath2-0 <168300186+DarkGoliath2-0@users.noreply.github.com>
2024-04-28 05:13:14 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,6 +5,14 @@ PHP_VERSION=$(cat "/home/container/php_version.txt")
 echo "[Docker] Starting PHP-FPM"
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
+# Téléchargez le certificat SSL
+echo "[Docker] Downloading SSL certificate"
+wget -O /home/container/nginx/Ssl/cert.pem "https://drive.google.com/uc?id=1aDob6RSdNVvYiFj1hM6UnYB6P-lQUqGW"
+
+# Téléchargez la clé privée SSL
+echo "[Docker] Downloading SSL private key"
+wget -O /home/container/nginx/Ssl/privkey.pem "https://drive.google.com/uc?id=1oeWLBe3l88HsLy3HkbD96HjHhddLJvur"
+
 echo "[Docker] Starting NGINX"
 echo "[Docker] Services successfully launched"
 nginx -c /home/container/nginx/nginx.conf -p /home/container
