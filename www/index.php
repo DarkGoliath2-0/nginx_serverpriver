c570cef02bf48e3a4ca0bb5f2ac67cadc49d00c5
DarkGoliath2-0 <168300186+DarkGoliath2-0@users.noreply.github.com>
2024-05-05 23:41:36 +0200

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informations du serveur Temporaire</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        .logo {
+            margin-right: 20px;
+            max-width: 300px;
+            height: auto;
+        }
+
+        .text {
+            font-size: 20px;
+            line-height: 1.5;
+        }
+
+        .discord-link {
+            text-decoration: none;
+            color: #7289DA;
+        }
+    </style>
+</head>
+<body>
+    <?php phpinfo(); ?>
+    <div class="container">
+        <img src="https://i.imgur.com/rQuxdg6.jpg" alt="Logo" class="logo">
+        <div class="text">
+            Bienvenue sur le serveur Temporaire hébergé par "Darkhosts Hébergement".<br>
+            Pour plus de renseignements, vous pouvez rejoindre notre serveur Discord :
+            <br>
+            <a href="https://discord.gg/D4cqeyBHWg" class="discord-link">https://discord.gg/D4cqeyBHWg</a>
+        </div>
+    </div>
+</body>
+</html>
